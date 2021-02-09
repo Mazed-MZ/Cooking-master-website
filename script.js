0fcc3cfb9560cde31943f2cfbcbfe15f2d2e8042
Mazed-MZ <mazedislam21@gmail.com>
2021-02-09 13:13:30 +0600

    itemdisplay

--- script.js
+++ script.js
@@ -3,17 +3,24 @@ fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast')
     .then(data => displayFoods(data.meals));
 
 const displayFoods = meals => {
-    const items = document.getElementById("foodList");
+    let items = document.getElementById("foodList");
     for (let i = 0; i < meals.length; i++) {
         const element = meals[i];
         const itemsDiv = document.createElement("div");
 
         itemsDiv.className = `foodDisplay`
         const foodInfo = `
+            <div onclick="foodDetails('${element.strMealThumb}')">
             <img class="img" src='${element.strMealThumb}'>
-            <h4 class="fontBox">${element.strMeal}</h4>
+            <h5 class="fontBox">${element.strMeal}</h5>
+            </div>
             `
         itemsDiv.innerHTML = foodInfo;
         items.appendChild(itemsDiv);
     }
+}
+const foodDetails = name =>{
+    const url = `${name}`
+    console.log(url)
+    
 }
